f42b004650b71d77ee164fc5e7bbceeac37814b5
unknown <codeswitharif@gmail.com>
2023-11-23 20:48:47 +0700

    task

---
+++ minggu8/latihan.php
@@ -0,0 +1,10 @@
+
+<?php
+
+echo"<h2>Menggunakan perulangan While</h2>";
+$i=10;
+do {
+    echo"Hello Word! <bf>";
+} while ($i < 5);
+?>
+#Hello Word!
---
+++ minggu8/latihan3.php
@@ -0,0 +1,10 @@
+<?php 
+$x = 20;
+if ($x < 10){
+    echo "benar";
+} elseif($x == 20) {
+    echo "BINGO";
+}else{
+    echo "salah";
+}
+?>
